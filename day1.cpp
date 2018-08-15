8e8360c976037888bd73c77fdffa6a7697ca4ecc
shashwatroy <shashwatroy007@gmail.com>
2018-08-15 16:09:50 +0530

    first challenge

---
+++ day1.cpp
@@ -0,0 +1,27 @@
+/*WAP which adds three user input numbers without using '+' operator*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int s,r,t,i,j,k,x,y,z,b,a=0;
+cout<<"Enter any three integers\n";
+cin>>x>>y>>z;
+r=(-x);
+s=(-y);
+t=(-z);
+ for(i=0;i>r;i--)
+  {a--;
+  }
+ for(j=0;j>s;j--)
+  {a--;
+  }
+ for(k=0;k>t;k--)
+  {a--;
+  }
+b=(-a);
+ cout<<"the sum of the three numbers is ";
+ cout<<b;
+return 0;
+}
